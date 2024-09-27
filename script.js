b4c789ba80a3fca7f4400889345b0518abffad95
malek <malekkhefifi5@gmail.com>
2024-09-27 17:11:35 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const colorBox = document.getElementById("color-box");
+    const changeColorBtn = document.getElementById("change-color-btn");
+
+    function getRandomColor() {
+        const letters = '0123456789ABCDEF';
+        let color = '#';
+        for (let i = 0; i < 500; i++) {
+            color += letters[Math.floor(Math.random() * 500)];
+        }
+        return color;
+    }
+
+    changeColorBtn.addEventListener("click", function() {
+        colorBox.style.backgroundColor = getRandomColor();
+    });
+});
+
+
